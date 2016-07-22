10d6b2a20e3399d5e26344eba9aa85b58970b185
Nick Langridge <nicklangridge@hotmail.com>
2016-07-22 16:29:57 +0100

    Here we go...

---
+++ plants/conf/SiteDefs.pm
@@ -0,0 +1,21 @@
+package EG::Archive::Plants::SiteDefs;
+use strict;
+
+
+sub update_conf {
+
+    $SiteDefs::ENSEMBL_PORT       = 8111;
+    $SiteDefs::ENSEMBL_SERVERNAME = 'archive.plants.ensembl.org';
+    $SiteDefs::SITE_NAME          = 'Ensembl Plants Archive';
+    $SiteDefs::ENSEMBL_SITETYPE          = 'Ensembl Plants Archive';
+    $SiteDefs::ENSEMBL_BASE_URL   = 'http://archive.plants.ensembl.org';
+    $SiteDefs::SITE_FTP           = 'ftp://ftp.ensemblgenomes.org/pub/release-31/plants/';
+
+    # Flag to enable/disable BLAST, VEP, Assembly Converter
+    $SiteDefs::ENSEMBL_BLAST_ENABLED  = 0;
+    $SiteDefs::ENSEMBL_VEP_ENABLED    = 0;
+    $SiteDefs::ENSEMBL_MART_ENABLED   = 0;
+    $SiteDefs::ENSEMBL_AC_ENABLED     = 0;
+}
+
+1;
